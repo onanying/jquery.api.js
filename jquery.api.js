871437107b44f3b87613003cd6b395a21a0c1119
Jian Liu <onanying@126.com>
2016-12-30 13:39:14 +0800

    增加token有效期控制

--- jquery.api.js
+++ jquery.api.js
@@ -4,21 +4,44 @@
 
     $[method] = new Object();
 
+    // 刷新token有效期
+    $[method].refreshExpires = function() {
+        var timestamp = new Date().getTime();        
+        if(sessionStorage.tokenLastRefresh) {
+            if(timestamp - sessionStorage.tokenLastRefresh > 600 * 1000){
+                // 刷新token
+                $.getJSON('/api/v1/account/refreshExpires?access_token=' + sessionStorage.access_token, function(json){
+                    if(json.errcode == 0){
+                        sessionStorage.tokenLastRefresh = timestamp;
+                    }
+                });
+            }
+        }else{
+            sessionStorage.tokenLastRefresh = timestamp;
+        }
+    }
+
     // 公共控制方法
     $[method].commonControl = function(json, success) {
 
     	/* 公共控制代码开始 */
+
+        // 错误码控制
         if (json.errcode == 30001 || json.errcode == 30002) {
             sessionStorage.clear(); // 清空用户数据
             window.location.href = '/'; // 跳转至首页
             return false;
         }
+
+        // token有效期控制
+        $[method].refreshExpires();
+
         /* 公共控制代码结束 */
 
         success(json);
     }
 
-    // jQuery ajax方法加入公共控制
+    // 重新封装jQuery.ajax方法
     $[method].ajax = function(conf) {
         var success = conf.success;
         conf.success = function(json) {
@@ -27,7 +50,7 @@
         $.ajax(conf);
     }
 
-    // jQuery getJSON方法加入公共控制
+    // 重新封装jQuery.getJSON方法
     $[method].getJSON = function() {
         switch (arguments.length) {
         case 2:
